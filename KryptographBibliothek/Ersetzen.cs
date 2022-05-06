410d2031595bb8da9c55c9720ef622af0ee5b01c
Ismael Zanati <ismael.z2@gso.schule.koeln>
2022-05-06 14:22:39 +0200

    Update

---
+++ KryptographBibliothek/Ersetzen.cs
@@ -0,0 +1,13 @@
+﻿// Remove trailing and leading white space.
+string source = "I'm wider than I need to be.";
+    // Store the results in a new string variable.
+    var trimmedResult = source.Trim();
+var trimLeading = source.TrimStart();
+var trimTrailing = source.TrimEnd();
+
+private readonly object p.system.Console.WriteLine11;
+
+private object Getp.system.Console.WriteLine()
+{
+    return this.p.system.Console.WriteLine11;
+}
