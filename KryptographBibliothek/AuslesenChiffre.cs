585a0ed968a841336ccf4b5250b95c5d1cfff868
Karim Lachgar <karim.l3@gso.schule.koeln>
2022-06-01 13:54:03 +0200

    bzz

---
+++ KryptographBibliothek/AuslesenChiffre.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace KryptographBibliothek
+{
+    public class AuslesenChiffre
+    {
+        public static void Auslesen()
+        { 
+            static void Main(string[] args)
+            {
+
+                string Chiffre = File.ReadAllText("C:\\Users\\karim.l3\\source\\repos\\Die_Cryptologen_Kryptograph\\Chiffre.txt");
+                Console.WriteLine("{0}", Chiffre);
+
+                //string Chiffre = File.ReadAllText(@"Chiffre.txt");
+                //Console.WriteLine("{0}", Chiffre);
+
+
+                //TextReader tr = new StreamReader(@"Chiffre.txt");
+                //string myText = tr.ReadLine();
+
+
+            }
+        }
+    }
+}
+
